93ee04f81450ed78c9cb8be799c5edeba93d9f56
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2014-08-27 11:27:02 -0700

    Integrate some 'Yeoman Best Practices'.

--- app/index.js
+++ app/index.js
@@ -1,17 +1,22 @@
 'use strict';
+var path = require('path');
+var chalk = require('chalk');
 var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
 
 var LittlestIsomorphGenerator = yeoman.generators.Base.extend({
   init: function () {
     this.pkg = require('../package.json');
+    this.props = {};
+    this.props.name = this._.slugify(this.appname || path.dirname(process.cwd()));
+    this.props.title = this._.titleize(this._.humanize(this.props.name));
+    this.props.description = 'A simple application built on Littlest-Isomorph, an isomorphic JavaScript stack.';
+  },
 
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies({
-          bower: false
-        });
-      }
-    });
+  welcome: function () {
+    if (!this.options['skip-welcome-message']) {
+      this.log(yosay('Welcome to the Littlest Isomorph Generator!'));
+    }
   },
 
   askFor: function () {
@@ -21,25 +26,36 @@ var LittlestIsomorphGenerator = yeoman.generators.Base.extend({
       {
         name: 'name',
         message: 'What is the name of this application?',
-        default: 'littlest-isomorph'
+        default: this.props.name
+      },
+      {
+        name: 'title',
+        message: 'What is the title of this application?',
+        default: this.props.title
+      },
+      {
+        name: 'description',
+        message: 'How would you describe this application?',
+        default: this.props.description
       }
     ];
 
     this.prompt(prompts, function (props) {
       this.props = props;
 
+      this.log();
       done();
     }.bind(this));
   },
 
   app: function () {
-    this.template('package.json', 'package.json', this.props);
+    this.template('_package.json', 'package.json', this.props);
     this.template('public/index.html', 'public/index.html', this.props);
     this.template('README.md', 'README.md', this.props);
 
     this.copy('.editorconfig', '.editorconfig');
     this.copy('_gitignore', '.gitignore');
-    this.copy('Gruntfile.js', 'Gruntfile.js');
+    this.copy('_Gruntfile.js', 'Gruntfile.js');
     this.copy('index.js', 'index.js');
     this.copy('bin/client', 'bin/client');
     this.copy('bin/server', 'bin/server');
@@ -51,6 +67,31 @@ var LittlestIsomorphGenerator = yeoman.generators.Base.extend({
     this.copy('lib/config.js', 'lib/config.js');
     this.copy('lib/router.js', 'lib/router.js');
     this.copy('styles/app.less', 'styles/app.less');
+  },
+
+  dependencies: function () {
+    if (!this.options['skip-install']) {
+      this.log();
+      this.log('Installing dependencies for you ' + chalk.bold('while you wait') + '!');
+      this.log(chalk.grey('(If this fails, try running ' + chalk.bold('npm install') + ' yourself.)'));
+      this.log();
+      this.installDependencies({
+        bower: false,
+        skipMessage: true,
+        callback: this.async()
+      });
+    }
+  },
+
+  farewell: function () {
+    this.log();
+    this.log(chalk.green('Application generated!'));
+    this.log('To get started:');
+    this.log('  grunt dev');
+    this.log('To read more:');
+    this.log('  https://github.com/Faithlife/littlest-isomorph');
+    this.log('  http://facebook.github.io/react/');
+    this.log();
   }
 });
 
